6d6962e7ec449c8050cc7fe571ff68e0e75545dc
anuja-joshi <anuja.joshi443@gmail.com>
2024-01-03 14:36:24 +0530

    updated api service

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: ''
+        key: '8703004e68c64cc5913dc00471a373b2'
     }
 })
