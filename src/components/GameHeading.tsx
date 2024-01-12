7df80177633f54e32e0490ced54950e2aa21a528
anuja-joshi <anuja.joshi443@gmail.com>
2024-01-12 19:04:34 +0530

    heading changes

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -8,7 +8,7 @@ interface Props {
 const GameHeading = ({ gameQuery }: Props) => {
   const heading = `${gameQuery.platform?.name || ""} ${
     gameQuery.genre?.name || ""
-  } Games (Exclusively by AJ)`;
+  } Games`;
   return (
     <Heading as="h1" marginY={5} fontSize="5xl">
       {heading}
